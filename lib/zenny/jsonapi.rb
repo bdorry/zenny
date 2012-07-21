42bc7922defa154b3397ce0b62cc4fbabedac0c4
bdorry <brian.dorry@gmail.com>
2012-07-20 22:58:14 -0400

    added descriptive error messages

--- lib/zenny/jsonapi.rb
+++ lib/zenny/jsonapi.rb
@@ -24,7 +24,7 @@ module Zenny
 
     # Check the HTTP and JSON response for errors and return JSON response
     def parse_json(resp)
-      #begin
+      begin
         puts resp.http_body.content
       
         if(resp.status != 200)
@@ -36,14 +36,15 @@ module Zenny
         # Check for JSON success. There are some exceptions where this doesn't make sense:
         #   1. Sometimes the 'success' key does not exist like in EventsRouter#query
         #   2. When json['result'] is not a Hash like a return from ReportRouter#get_tree
-        if(json['result'].is_a?(Hash) && json['result'].has_key?('success') && !json['result']['success'])
-          raise ZenossError, "JSON request '#{json['method']}' on '#{json['action']}' was unsuccessful"
+          
+        if json['result']['success'] == false
+          raise ZenossError, "JSON request '#{json['method']}' on '#{json['action']}' was unsuccessful: #{json['result']['msg']}"          
         end
 
         json['result']
-      #rescue JSON::ParserError => e
-      #  raise ZenossError, "Invalid JSON response: #{e.message}"
-      #end
+      rescue JSON::ParserError => e
+        raise ZenossError, "Invalid JSON response: #{e.message}"
+      end
     end
 
   end
